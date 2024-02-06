94174f67a23e6024950585dc85d81180b561bdb8
Codessor <FuriaWhite@yandex.ru>
2024-02-06 18:53:06 +0500

    Переписанная версия с Jsx на JS

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import logo from "./logo.svg"; // императивный стиль начало
+import logo from "./logo.svg";
 import "./App.css";
-import Year from './components/getYear';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a> {/* императивный стиль конец */}
-				<Year /> {/* строка декларативного стиля */}
-			</header> {/* императивный стиль начало */}
-		</div>
-	);
+	const appDiv = document.createElement("div");
+	appDiv.className = "App";
+
+	const appHeader = document.createElement("header");
+	appHeader.className = "App-header";
+
+	const appHeaderImg = document.createElement("img");
+	appHeaderImg.src = logo;
+	appHeaderImg.className = "App-logo";
+	appHeaderImg.alt = "logo";
+
+	const appHeaderP = document.createElement("p");
+	appHeaderP.innerHTML = "Edit <code>src/App.js</code> and save to reload.";
+
+	const appHeaderLink = document.createElement("a");
+	appHeaderLink.clasName = "App-link";
+	appHeaderLink.href = "https://reactjs.org";
+	appHeaderLink.target = "_blank";
+	appHeaderLink.rel = "noopener noreferrer";
+	appHeaderLink.textContent = "Learn React";
+
+	const appHeaderDate = document.createElement("p");
+	appHeaderDate.textContent = new Date().getFullYear();
+
+	appHeader.append(appHeaderImg);
+	appHeader.append(appHeaderP);
+	appHeader.append(appHeaderLink);
+	appHeader.append(appHeaderDate);
+
+	appDiv.append(appHeader);
+	return appDiv;
 }
 
-export default App;  // императивный стиль конец
+export default App;
