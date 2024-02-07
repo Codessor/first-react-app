834503d4ec500ba46cf685734b2119ab7bb2a176
Codessor <FuriaWhite@yandex.ru>
2024-02-07 15:43:16 +0500

    JSX -> JS v2/ createElemet from React

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { createElement } from "react";
 
 function App() {
-	const appDiv = document.createElement("div");
-	appDiv.className = "App";
-
-	const appHeader = document.createElement("header");
-	appHeader.className = "App-header";
-
-	const appHeaderImg = document.createElement("img");
-	appHeaderImg.src = logo;
-	appHeaderImg.className = "App-logo";
-	appHeaderImg.alt = "logo";
-
-	const appHeaderP = document.createElement("p");
-	appHeaderP.innerHTML = "Edit <code>src/App.js</code> and save to reload.";
-
-	const appHeaderLink = document.createElement("a");
-	appHeaderLink.clasName = "App-link";
-	appHeaderLink.href = "https://reactjs.org";
-	appHeaderLink.target = "_blank";
-	appHeaderLink.rel = "noopener noreferrer";
-	appHeaderLink.textContent = "Learn React";
-
-	const appHeaderDate = document.createElement("p");
-	appHeaderDate.textContent = new Date().getFullYear();
-
-	appHeader.append(appHeaderImg);
-	appHeader.append(appHeaderP);
-	appHeader.append(appHeaderLink);
-	appHeader.append(appHeaderDate);
-
-	appDiv.append(appHeader);
-	return appDiv;
+	return createElement(
+		"div",
+		{ className: "App" },
+		createElement(
+			"header",
+			{ className: "App-header" },
+			createElement("img", {
+				className: "App-logo",
+				src: logo,
+				alt: "logo",
+			}),
+			createElement(
+				"p",
+				null,
+				"Edit",
+				createElement("code", "src/App.js"),
+				" and save to reload."
+			),
+			createElement(
+				"a",
+				{
+					clasName: "App-link",
+					href: "https://reactjs.org",
+					target: "_blank",
+					rel: "noopener noreferrer",
+				},
+				"Learn React"
+			),
+			createElement("p", null, new Date().getFullYear())
+		)
+	);
 }
 
 export default App;
